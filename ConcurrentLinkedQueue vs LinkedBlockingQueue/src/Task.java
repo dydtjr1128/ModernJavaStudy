ad9b6a42c8e1d9857d79d38d63adab5d3bca3caf
Choi Yong Seok <dydtjr1994@gmail.com>
2020-10-13 19:13:26 +0900

    Add: ConcurrentLinkedQueue vs LinkedBlockingQueue 코드 추가.

---
+++ ConcurrentLinkedQueue vs LinkedBlockingQueue/src/Task.java
@@ -0,0 +1,26 @@
+import java.util.Queue;
+import java.util.concurrent.TimeUnit;
+
+public class Task implements Runnable {
+    private final int taskId;
+    private final Queue<String> queue;
+
+    public Task(int id, Queue<String> queue) {
+        this.taskId = id;
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        int i=0;
+        while (true) {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return;
+            }
+            queue.offer("Task id : " + taskId + " Loop : " + i);
+        }
+    }
+}
