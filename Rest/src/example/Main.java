dd337026178265afe7cefd182ee0f7987f0db753
Choi Yong Seok <dydtjr1994@gmail.com>
2020-10-13 16:26:09 +0900

    Add: rest 코드 추가

---
+++ Rest/src/example/Main.java
@@ -0,0 +1,10 @@
+package example;
+
+/**
+ * @author : dydtjr1128
+ * @project : Study
+ * @github : https://github.com/dydtjr1128
+ * @since : 2020-08-14
+ */
+public class Main {
+}
---
+++ Rest/src/main/java/com/example/rest/ExampleController.java
@@ -0,0 +1,10 @@
+package com.example.rest;
+
+/**
+ * @author : dydtjr1128
+ * @project : Study
+ * @github : https://github.com/dydtjr1128
+ * @since : 2020-08-14
+ */
+public class ExampleController {
+}
