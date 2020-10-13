8ccb2d93fdd72478b53e106c598afe8c813c6131
Choi Yong Seok <dydtjr1994@gmail.com>
2020-10-13 18:31:33 +0900

    Add:Thread 관련 코드 추가

---
+++ ThreadPool/src/Main.java
@@ -0,0 +1,61 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        int threadCount = 10;
+
+        System.out.println("===Executors.newFixedThreadPool===");
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.shutdown();
+        for (int i = 0; i < threadCount; i++) {
+            executorService.execute(new MyRunnable(i));
+            System.out.println("tt");
+        }
+        while (true){
+            TimeUnit.SECONDS.sleep(1500);
+            System.out.println("!");
+            break;
+        }
+
+        /*try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("===Executors.newCachedThreadPool===");
+        executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < threadCount; i++) {
+            executorService.execute(new MyRunnable(i));
+        }
+
+
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("===Executors.newSingleThreadExecutor===");
+        executorService = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < threadCount; i++) {
+            executorService.execute(new MyRunnable(i));
+        }
+
+
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("===Executors.newScheduledThreadPool===");
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        for (int i = 0; i < threadCount; i++) {
+            scheduledExecutorService.scheduleAtFixedRate(new MyRunnable(i), 0, 1, TimeUnit.SECONDS); // scheduleAtFixedRate 호출 당 큐 하나 생김. 여기 말고 MyRunnable 내부에 for문 거는것-> 큐하나로 돌려쓰기 때문에 대기함.
+        }*/
+
+
+
+    }
+}
--- ThreadPool/src/MyRunnable.java
+++ ThreadPool/src/MyRunnable.java
@@ -1,4 +1,4 @@
-package PACKAGE_NAME;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author : dydtjr1128
@@ -6,5 +6,22 @@ package PACKAGE_NAME;
  * @github : https://github.com/dydtjr1128
  * @since : 2019-12-12
  */
-public class MyRunnable {
+public class MyRunnable implements Runnable {
+    int id;
+
+    MyRunnable(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        long st = 0;
+        try {
+            st = System.currentTimeMillis();
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("hello" + id + " " + Thread.currentThread() + Thread.activeCount() + " " + (System.currentTimeMillis() - st));
+    }
 }
