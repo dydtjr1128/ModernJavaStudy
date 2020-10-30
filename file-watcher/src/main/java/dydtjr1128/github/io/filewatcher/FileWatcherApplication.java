60d082e11f82e6aa6e208b55e38879b6390fbdc6
Yongseok Choi <dydtjr1994@gmail.com>
2020-10-30 14:38:24 +0900

    Add: FileWatcher 예제 모듈 추가

---
+++ file-watcher/src/main/java/dydtjr1128/github/io/filewatcher/FileWatcherApplication.java
@@ -0,0 +1,13 @@
+package dydtjr1128.github.io.filewatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileWatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileWatcherApplication.class, args);
+	}
+
+}
---
+++ file-watcher/src/test/java/dydtjr1128/github/io/filewatcher/FileWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package dydtjr1128.github.io.filewatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileWatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
